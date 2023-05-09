c09a13cab96f0c1c18601cda0928b597291993ab
Justin CT Liu <justintwd@gmail.com>
2023-05-09 16:00:24 +0800

    chore: update config

--- configs/apps/.oh-my-zsh/custom/example-variables/variables-justinmbp.zsh
+++ configs/apps/.oh-my-zsh/custom/example-variables/variables-justinmbp.zsh
@@ -1,5 +1,7 @@
 export PATH="${HOME}/.pulumi/bin:$PATH"
 export PATH="${KREW_ROOT:-${HOME}/.krew}/bin:$PATH"
 export PATH="/usr/local/Cellar/mtr/0.95/sbin/:$PATH"
-export DOCKER_HOST=tcp://JustinPC:2375
 export PATH="/Users/justinct_liu/.rd/bin:$PATH"
+export PATH="/Applications/VMware OVF Tool/:$PATH"
+
+export DOCKER_HOST=tcp://JustinPC:2375
