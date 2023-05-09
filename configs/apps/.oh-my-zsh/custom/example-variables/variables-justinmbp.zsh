0f0072bab516ec97f6dbd1d4af6450893ce7cc7f
Justin CT Liu <justintwd@gmail.com>
2023-05-09 16:00:24 +0800

    chore: update config
    chore: remove usless package
    
    chore: remove zsh-nvm plugin
    
    chore: add pnpm
    
    chore: update p10k config
    
    chore: add font for p10k
    
    Revert "chore: add font for p10k"
    
    This reverts commit 67d05e8a4499eac929cd9affa916bcb3fc49d7f8.
    
    chore: add front for p10k
    
    chore: update python
    
    chore: date packages
    
    chore: change docker host
    
    chore: update python
    
    chore: upgrade
    
    chore: fix scripts
    
    chore: update vscode
    
    chore: update deps
    
    chore: fix install kctx failed
    
    chore: update configs
    
    chore: update editor
    
    chore: add backup
    
    chore: add backup
    
    chore: add backup
    
    chore: use volta
    
    chore: support arm64
    
    chore: add install aws session-manager
    
    chore: docker compose config
    
    chore: support vscode
    
    chore: upgrade pkgs
    
    chore: upgrade pkgs
    
    chore: change font to maple mono
    
    chore update configs
    
    chore: update configs
    
    chore: update configs
    
    chore: update configs
    
    chore: update configs
    
    Signed-off-by: Justin CT Liu <justintwd@gmail.com>
    
    chore: update puppet
    
    Signed-off-by: Justin CT Liu <justintwd@gmail.com>

--- configs/apps/.oh-my-zsh/custom/example-variables/variables-justinmbp.zsh
+++ configs/apps/.oh-my-zsh/custom/example-variables/variables-justinmbp.zsh
@@ -1,5 +1,9 @@
 export PATH="${HOME}/.pulumi/bin:$PATH"
 export PATH="${KREW_ROOT:-${HOME}/.krew}/bin:$PATH"
 export PATH="/usr/local/Cellar/mtr/0.95/sbin/:$PATH"
-export DOCKER_HOST=tcp://JustinPC:2375
-export PATH="/Users/justinct_liu/.rd/bin:$PATH"
+export PATH="${HOME}/.rd/bin:$PATH"
+export PATH="/Applications/VMware OVF Tool/:$PATH"
+export PATH="${HOME}/Library/pnpm:$PATH"
+export PATH="${HOME}/.local/bin:$PATH"
+export PATH="/usr/local/bin:$PATH"
+export DOCKER_HOST=unix:///var/run/docker.sock
