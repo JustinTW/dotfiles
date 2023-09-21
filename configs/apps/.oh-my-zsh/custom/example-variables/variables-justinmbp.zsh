70149f394ccf72a737338d3fe40835d4090b38d3
Justin CT Liu <justintwd@gmail.com>
2023-09-21 14:32:32 +0800

    chore: change docker host

--- configs/apps/.oh-my-zsh/custom/example-variables/variables-justinmbp.zsh
+++ configs/apps/.oh-my-zsh/custom/example-variables/variables-justinmbp.zsh
@@ -4,4 +4,4 @@ export PATH="/usr/local/Cellar/mtr/0.95/sbin/:$PATH"
 export PATH="/Users/justinct_liu/.rd/bin:$PATH"
 export PATH="/Applications/VMware OVF Tool/:$PATH"
 export PATH="${HOME}/Library/pnpm:$PATH"
-export DOCKER_HOST=tcp://localhost:2375
+export DOCKER_HOST=unix:///var/run/docker.sock
