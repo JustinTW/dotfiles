8be37ea1bc0930fa83ed16046e19c47e85a1d346
Justin CT Liu <justintwd@gmail.com>
2023-12-13 20:33:56 +0800

    chore: update python

--- configs/apps/.oh-my-zsh/custom/example-variables/variables-justinmbp.zsh
+++ configs/apps/.oh-my-zsh/custom/example-variables/variables-justinmbp.zsh
@@ -4,4 +4,5 @@ export PATH="/usr/local/Cellar/mtr/0.95/sbin/:$PATH"
 export PATH="/Users/justinct_liu/.rd/bin:$PATH"
 export PATH="/Applications/VMware OVF Tool/:$PATH"
 export PATH="${HOME}/Library/pnpm:$PATH"
+export PATH="/Users/justinct_liu/.local/bin:$PATH"
 export DOCKER_HOST=unix:///var/run/docker.sock
--- configs/apps/.zshrc
+++ configs/apps/.zshrc
@@ -52,3 +52,5 @@ esac
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+
+export JAVA_HOME=/Applications/Android\ Studio.app/Contents/jbr/Contents/Home
