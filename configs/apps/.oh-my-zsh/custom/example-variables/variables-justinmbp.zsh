3c39130ad76bbcc98ed3728adfaeac6369d95c1c
JustinTW <justintwd@gmail.com>
2024-01-21 21:55:37 +0800

    chore: upgrade

--- configs/apps/.oh-my-zsh/custom/example-variables/variables-justinmbp.zsh
+++ configs/apps/.oh-my-zsh/custom/example-variables/variables-justinmbp.zsh
@@ -1,8 +1,8 @@
 export PATH="${HOME}/.pulumi/bin:$PATH"
 export PATH="${KREW_ROOT:-${HOME}/.krew}/bin:$PATH"
 export PATH="/usr/local/Cellar/mtr/0.95/sbin/:$PATH"
-export PATH="/Users/justinct_liu/.rd/bin:$PATH"
+export PATH="${HOME}/.rd/bin:$PATH"
 export PATH="/Applications/VMware OVF Tool/:$PATH"
 export PATH="${HOME}/Library/pnpm:$PATH"
-export PATH="/Users/justinct_liu/.local/bin:$PATH"
+export PATH="${HOME}/.local/bin:$PATH"
 export DOCKER_HOST=unix:///var/run/docker.sock
