7a7b544ea771845ebd619902a2af43472662a9f6
Justin CT Liu <justintwd@gmail.com>
2025-09-04 14:31:53 +0800

    chore: update configs

--- configs/apps/.oh-my-zsh/custom/example-variables/variables-justinmbp.zsh
+++ configs/apps/.oh-my-zsh/custom/example-variables/variables-justinmbp.zsh
@@ -7,3 +7,13 @@ export PATH="${HOME}/Library/pnpm:$PATH"
 export PATH="${HOME}/.local/bin:$PATH"
 export PATH="/usr/local/bin:$PATH"
 export DOCKER_HOST=unix:///var/run/docker.sock
+
+
+# Anthropic API Configuration
+export ANTHROPIC_BASE_URL="https://api5.ai"
+# export ANTHROPIC_BASE_URL="http://localhost:3001"
+export ANTHROPIC_BASE_URL="https://proxy.hfct.online/"
+
+# 36
+export ANTHROPIC_AUTH_TOKEN="sk-iZDHIMQTUTQQQSSTUKMQQUWWY0569dmkdgihijln"
+export ANTHROPIC_API_KEY="sk-iZDHIMQTUTQQQSSTUKMQQUWWY0569dmkdgihijln"
